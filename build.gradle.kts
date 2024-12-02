7c1e31fb635f0592d4a326e4aeb73992fad69578
Cristóbal Veas <cristobal.veas@proton.me>
2024-12-02 02:16:22 -0300

    build: add unnamed/inject dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,11 @@ version = "1.0.0"
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("team.unnamed:inject:2.0.1")
+}
