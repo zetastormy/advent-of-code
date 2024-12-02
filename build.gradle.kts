c8576d5ca2e548b2cc1fad5ebcea691fa7d61df8
Cristóbal Veas <cristobal.veas@proton.me>
2024-12-02 00:19:41 -0300

    feat: modify gradle files to fit our needs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,9 @@ plugins {
     id("java")
 }
 
-group = "me.zetastormy.aoc"
+group = "me.zetastormy"
 version = "1.0.0"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
